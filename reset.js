412dd10b0b0f9125675c250f6ea68c089fc3ac47
Arif <kudaayey@gmail.com>
2024-07-15 08:57:14 +0700

    menambahkan fungsi untuk menghapus seluruh data dari database dalam tabel produk

--- reset.js
+++ reset.js
@@ -1,7 +1,7 @@
 function hapusDataNota() {
     if (confirm('Anda yakin ingin menghapus semua data nota?')) {
         // Buat permintaan AJAX ke server
-        fetch('reset_penjualan.php', {
+        fetch('reset_penjualan_Nota.php', {
             method: 'POST',
         })
         .then(response => response.text())
@@ -19,3 +19,25 @@ function hapusDataNota() {
         });
     }
 }
+function hapusDataProduk() {
+    if (confirm('Anda yakin ingin menghapus semua data Produk?')) {
+        // Buat permintaan AJAX ke server
+        fetch('reset_penjualan_Produk.php', {
+            method: 'POST',
+        })
+        .then(response => response.text())
+        .then(data => {
+            if (data.trim() === "success") {
+                alert('Data Produk berhasil dihapus.');
+                window.location.reload();
+            } else {
+                alert('Terjadi kesalahan saat menghapus data Produk.');
+            }
+        })
+        .catch(error => {
+            alert('Terjadi kesalahan koneksi.');
+            console.error('Error:', error);
+        });
+    }
+}
+
